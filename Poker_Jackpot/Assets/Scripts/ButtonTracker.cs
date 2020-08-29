83527b1a8d1729d2785624a5d93c324ea2d05ff8
LAPTOP-1MC7F58L\suresh <sureshben78420@gmail.com>
2020-08-29 18:04:57 +0530

    SplashScreenSetup

---
+++ Poker_Jackpot/Assets/Scripts/ButtonTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonTracker : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void instructions()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Poker_Jackpot/Assets/Scripts/InitialScreenScrip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialScreenScrip : MonoBehaviour
+{
+    [SerializeField] GameObject ButtonCanvas;
+    void Start() 
+    {
+        Invoke("ButtonScreenLoading", 2f);
+    }
+
+    void ButtonScreenLoading()
+    {
+        ButtonCanvas.SetActive(true);
+    }
+
+}
